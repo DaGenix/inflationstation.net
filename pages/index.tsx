62af3ffa2e3232f81f5cfbef96ea076055a5b13f
Palmer Cox <palmer@fiscalnote.com>
2021-07-20 00:35:10 -0400

    Add some explanatory text about the prices we are displaying

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ export default function HomePage(props: HomePageProps) {
                 </noscript>
             </Head>
 
-            <Header/>
+            <Header data={data} />
 
             <Content
                 key={componentKey}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,19 @@
 import {Box} from "@material-ui/core";
+import {DataType} from "../util/data";
 
-export default function Header() {
+type HeaderProps = {
+    data: DataType,
+}
+
+export default function Header({data}: HeaderProps) {
+    const switchItem = data.data.find(i => i.name === "Switch");
+    if (!switchItem) {
+        throw new Error("Couldn't find item for Switch");
+    }
+    const switchYear = switchItem.year;
+    const origSwitchPrice = switchItem.orig_prices[0];
+    const todaysSwitchPrice = switchItem.prices[0];
+    const switchDiscount = (switchItem.raw_prices[0] - switchItem.raw_orig_prices[0]).toLocaleString();
     return (
         <header>
             <Box
@@ -9,9 +22,21 @@ export default function Header() {
                     backgroundColor: "#6c4d70",
                     textAlign: "center",
                     py: 2,
+                    px: 1,
                 }}
             >
                 <h1>Console Prices Adjusted for Inflation</h1>
+                <p>
+                    Prices show have been adjusted for inflation since the consoles
+                    were initially released. Some consoles are still available at the same price
+                    they were released at. Due to inflation, these consoles have effectively
+                    become cheaper even though their price hasn't changed. For example, the
+                    Switch was released in {switchYear} for ${origSwitchPrice}.
+                    Due to inflation, ${origSwitchPrice} of goods in {switchYear} would now
+                    cost ${todaysSwitchPrice}. Since the Switch's price hasn't changed,
+                    its effectively become ${switchDiscount} cheaper than it was when
+                    it was released.
+                </p>
             </Box>
         </header>
     );
