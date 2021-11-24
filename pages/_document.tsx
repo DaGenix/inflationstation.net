25ef8bacfa4cc45ee9c5a9408ee1762a294ad7fc
Palmer Cox <palmer@fiscalnote.com>
2021-11-24 18:28:45 -0500

    Enable Google Analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,19 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-213667339-1">
+            </script>
+            <script>
+              {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'UA-213667339-1');
+              `}
+            </script>
+        </Head>
         <body style={{backgroundColor: "#f1f1f1"}}>
           <Main />
           <NextScript />
