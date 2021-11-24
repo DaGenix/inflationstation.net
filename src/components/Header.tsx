eb9f157ae5b2d83e319731d6a0a76fb6df58b33c
Palmer Cox <palmer@fiscalnote.com>
2021-11-24 14:39:07 -0500

    Hide the long description of the site by default

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
-import {Box} from "@mui/material";
+import {Box, Typography} from "@mui/material";
 import {DataType} from "../util/data";
+import {useState} from "react";
 
 type HeaderProps = {
     data: DataType,
 }
 
 export default function Header({data}: HeaderProps) {
+    const [isOpen, setOpen] = useState(false);
     const switchItem = data.data.find(i => i.names[0] === "Nintendo Switch");
     if (!switchItem) {
         throw new Error("Couldn't find item for Switch");
@@ -26,17 +28,47 @@ export default function Header({data}: HeaderProps) {
                 }}
             >
                 <h1>Console Prices Adjusted for Inflation</h1>
+                {isOpen &&
+                    <p>
+                        Prices shown have been adjusted for inflation since the consoles
+                        were initially released. Some consoles are still available at the same price
+                        they were released at. Due to inflation, these consoles have effectively
+                        become cheaper even though their price hasn't changed. For example, the
+                        Switch was released in {switchYear} for ${origSwitchPrice}.
+                        Due to inflation, ${origSwitchPrice} of goods in {switchYear} would now
+                        cost ${todaysSwitchPrice}. Since the Switch's price hasn't changed,
+                        its effectively become ${switchDiscount} cheaper than it was when
+                        it was released.&nbsp;
+                        <Typography
+                            color="white"
+                            component="a"
+                            href="#"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                setOpen(false);
+                            }}
+                        >
+                            Less
+                        </Typography>
+                    </p>
+                }
+                {!isOpen &&
                 <p>
                     Prices shown have been adjusted for inflation since the consoles
-                    were initially released. Some consoles are still available at the same price
-                    they were released at. Due to inflation, these consoles have effectively
-                    become cheaper even though their price hasn't changed. For example, the
-                    Switch was released in {switchYear} for ${origSwitchPrice}.
-                    Due to inflation, ${origSwitchPrice} of goods in {switchYear} would now
-                    cost ${todaysSwitchPrice}. Since the Switch's price hasn't changed,
-                    its effectively become ${switchDiscount} cheaper than it was when
-                    it was released.
+                    were initially released...&nbsp;
+                    <Typography
+                        color="white"
+                        component="a"
+                        href="#"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setOpen(true);
+                        }}
+                    >
+                        More
+                    </Typography>
                 </p>
+                }
             </Box>
         </header>
     );
