e5bc76b2f825f095504ee53bdb94fdeac58626ca
Palmer Cox <palmer@fiscalnote.com>
2022-10-16 16:26:26 -0400

    Add link rel="canonical" element since Google asked me too

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,6 +30,7 @@ export default class MyDocument extends Document {
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
             <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&display=swap" rel="stylesheet" />
+            <link rel="canonical" href="https://www.inflationstation.net/" />
         </Head>
         <body>
           <Main />
